2dba90e72f05e597d20fe484fcf87491ed30ea7d
ashiruhammed <hammedashiru89@gmail.com>
2025-04-21 08:56:08 +0100

    feat: integrate TypeORM with SQLite database
    - Added TypeORM and SQLite dependencies to package.json
    - Configured TypeOrmModule in app.module.ts to use SQLite with a database file named 'db.sqlite'
    - Created an empty SQLite database file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { ReportModule } from './report/report.module';
 import { ReportService } from './report/report.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, ReportModule],
+  imports: [
+    UserModule,
+    ReportModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ReportService],
 })
