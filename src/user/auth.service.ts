1df9d29a5100d74c53dccebd34a66f949968e198
ashiruhammed <hammedashiru89@gmail.com>
2025-04-24 14:37:53 +0100

    feat: enhance AuthService with error handling for signUp and signIn methods

--- src/user/auth.service.ts
+++ src/user/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { scrypt as _scrypt, randomBytes } from 'crypto';
 import { promisify } from 'util';
 import { UserService } from './user.service';
@@ -10,10 +10,32 @@ export class AuthService {
   constructor(private userService: UserService) {}
 
   async signUp(email: string, password: string) {
+    // Check if the email is already in use
+    const existingUser = await this.userService.find(email);
+    if (existingUser) {
+      throw new BadRequestException('Email in use');
+    }
+
     const salt = randomBytes(8).toString('hex');
     const hash = (await scrypt(password, salt, 32)) as Buffer;
     const hashedPassword = `${salt}.${hash.toString('hex')}`;
 
     return this.userService.create(email, hashedPassword);
+  }
+
+  async signIn(email: string, password: string) {
+    const user = await this.userService.find(email);
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    return user;
   }
 }
